2c8ead47fb7b61d84673f3462dac9f3fb507b870
Benjamin Winkler <abwinkler999@gmail.com>
2014-04-02 18:19:57 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Serapeum::Application.config.secret_token = 'c8c0bdaa941f8a434ff3e6269257411468eb9a7d45bb3f30b6dafe2f98c46c99de6a7b5c7dd3e26531ee8e0f13d22d5c44672d65f206b940df7b54b2b3dfe1b4'
