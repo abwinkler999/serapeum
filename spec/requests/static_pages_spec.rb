042ecfc1dbf9523e2c3596d9f1c190192464c0f9
Benjamin Winkler <abwinkler999@gmail.com>
2014-08-13 23:56:24 -0400

    Poking at tests

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -7,5 +7,10 @@ describe "StaticPages" do
       visit '/'
       page.should have_content('Serapeum Card Catalog')
     end
+
+    it "should display a random book on the index page" do
+    	visit '/'
+    	page.should have_content(', by')
+    end
   end
 end
